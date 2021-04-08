bc53eb37b886d9ef14e163520c656b41202b1871
Alexei Ignatkevich <59063055+WizzArt0@users.noreply.github.com>
2021-04-08 22:23:29 +0300

    Add recommended swiper and category "food & drink"

--- js/main.js
+++ js/main.js
@@ -9,3 +9,27 @@ const swiper = new Swiper('.channel-slider', {
     },
   
   });
+
+const recommended_swiper = new Swiper('.recommended-slider', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 3,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.recommended-video-button-next',
+      prevEl: '.recommended-video-button-prev',
+    },
+  
+  });
+
+const category_swiper = new Swiper('.category-slider', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 5,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.category-button-next',
+      prevEl: '.category-button-prev',
+    },
+  
+  });
